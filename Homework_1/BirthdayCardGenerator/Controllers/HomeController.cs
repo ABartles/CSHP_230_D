87dd6a3133157fb8d59eb6f93655cd5cd7abfb0b
ABartles <andrew.j.bartles@gmail.com>
2017-04-24 16:42:50 -0700

    homework 1

---
+++ Homework_1/BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CreateCard
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardForm(Models.CardFill cardfill)
+        {
+            // If Statment added for entry validation
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", cardfill);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Homework_1/BirthdayCardGenerator/Models/CardFill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; // Added for entry validation
+
+namespace BirthdayCardGenerator.Models
+{
+    public class CardFill
+    {
+        
+        [Required(ErrorMessage = "Please enter From")] // Entry validation
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")] // Entry validation
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")] // Entry validation
+        public string Message { get; set; }
+    }
+}
