7f530822fd6a71c6f486868967e9ee5c4ff4c358
ABartles <andrew.j.bartles@gmail.com>
2017-04-26 08:46:55 -0700

    In Class Work

---
+++ HW2/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Action Method corisponding to the two links on the "Index" view
+        // User is making a request or  "HttpGET" aka GET
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        // Action Method corisponding to the submit button on "RsvpForm"
+        // User is pushing data or "HttpPost" 
+        // 1st Parameter: a model, 2nd Parameter: the model data
+        [HttpPost]
+        public ActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            //  if statement added as part of validation to prevent progress to next pg
+            if (ModelState.IsValid)
+            {
+                // 1st Parameter: is the view (displayed), 2nd Parameter: is the model data 
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ HW2/HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;    // Added to allow validation below
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")] // Validation
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")] // Validation
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")] // Validation
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Please enter attendance")] // Validation
+        public bool? WillAttend { get; set; }
+    }
+}
